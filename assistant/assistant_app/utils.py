4d3c915f866554623e74899be7b1a863880148dd
Aalekh Sajonia <aalekhsajonia@UNBLR-MIC-01513.local>
2024-05-02 09:47:43 +0530

    one more

--- assistant/assistant_app/utils.py
+++ assistant/assistant_app/utils.py
@@ -6,10 +6,10 @@ def create_random_uid(size=16, chars=string.digits + string.ascii_uppercase):
     return ''.join(random.choice(chars) for _ in range(size))
 
 def handle_uploaded_file(f, name):
-    if os.getenv("FILE_UPLOAD_PATH", None):
-        destination = os.getenv("FILE_UPLOAD_PATH", None) + name + '.txt'
-    else:
-        destination = os.path.join(os.path.dirname(os.path.dirname(__file__)),'assistant_app/uploaded_files/' + name + '.txt')
+    # if os.getenv("FILE_UPLOAD_PATH", None):
+    destination = "/tmp/" + name + '.txt'
+    # else:
+    #     destination = os.path.join(os.path.dirname(os.path.dirname(__file__)),'assistant_app/uploaded_files/' + name + '.txt')
     with open(destination, "wb+") as destination:
         for chunk in f.chunks():
             destination.write(chunk)
