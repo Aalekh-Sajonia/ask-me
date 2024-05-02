6091d1dd0ebb6f34b0afb67a3f8efe6df1ad1254
Aalekh Sajonia <aalekhsajonia@UNBLR-MIC-01513.local>
2024-05-02 14:42:25 +0530

    added new file type

--- assistant/assistant_app/utils.py
+++ assistant/assistant_app/utils.py
@@ -6,7 +6,11 @@ def create_random_uid(size=16, chars=string.digits + string.ascii_uppercase):
     return ''.join(random.choice(chars) for _ in range(size))
 
 def handle_uploaded_file(f, name):
-    destination = os.path.join(os.path.dirname(os.path.dirname(__file__)),'assistant_app/uploaded_files/' + name + '.txt')
+    split_tup = os.path.splitext(str(f))
+    print(split_tup)
+    if len(split_tup) < 2: 
+        raise Exception()
+    destination = os.path.join(os.path.dirname(os.path.dirname(__file__)),'assistant_app/uploaded_files/' + name + split_tup[1])
     with open(destination, "wb+") as destination:
         for chunk in f.chunks():
             destination.write(chunk)
